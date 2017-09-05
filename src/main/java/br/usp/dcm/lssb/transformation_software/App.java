da778bbe2e2402393fcb2dac72e7fbca8535bdcb
Gabriel Gusmao <gcsgpp@gmail.com>
2017-09-05 16:06:29 -0300

    Initializing Maven

---
+++ src/main/java/br/usp/dcm/lssb/transformation_software/App.java
@@ -0,0 +1,13 @@
+package br.usp.dcm.lssb.transformation_software;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

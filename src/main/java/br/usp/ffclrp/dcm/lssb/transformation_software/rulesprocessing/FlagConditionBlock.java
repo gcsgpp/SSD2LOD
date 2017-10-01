609368fab928da631a1c231639be5a40932746f2
Gabriel Gusmao <gcsgpp@gmail.com>
2017-10-01 09:28:43 -0300

    New flag type: Condition Block

---
+++ src/main/java/br/usp/ffclrp/dcm/lssb/transformation_software/rulesprocessing/FlagConditionBlock.java
@@ -0,0 +1,10 @@
+package br.usp.ffclrp.dcm.lssb.transformation_software.rulesprocessing;
+
+public class FlagConditionBlock extends Flag {
+	private int id;
+	public FlagConditionBlock() {
+	}
+	public FlagConditionBlock(int id) {
+		this.id = id;
+	}
+}

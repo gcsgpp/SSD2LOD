b14780f86475e18b6aa33514942a8a2dbcc4d573
Unknown <gcsgpp@gmail.com>
2018-03-23 16:46:55 -0300

    Remove transformation_manager and API files (project separation)

--- src/main/java/br/usp/ffclrp/dcm/lssb/custom_exceptions/DirectoryCreationFailedException.java
+++
@@ -1,4 +0,0 @@
-package br.usp.ffclrp.dcm.lssb.custom_exceptions;
-
-public class DirectoryCreationFailedException extends Exception {
-}
--- src/main/java/br/usp/ffclrp/dcm/lssb/custom_exceptions/TransformationActivityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.usp.ffclrp.dcm.lssb.custom_exceptions;
-
-public class TransformationActivityNotFoundException extends Exception{
-
-    public TransformationActivityNotFoundException(String id){
-        super("Transformation ID: " + id + " not found.");
-    }
-}
